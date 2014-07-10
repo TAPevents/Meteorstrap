5d2018c12cc1a578e638d7c82b1aee4b3ef08ea8
Chris Hitchcott <hitchcott@gmail.com>
2014-07-10 20:34:40 +0800

    Minified getPackageAssets

--- package.js
+++ package.js
@@ -4,44 +4,8 @@ Package.describe({
 
 Npm.depends({'less':'1.7.3'})
 
-var getPackageAssets = function(packageName,folder){
-    // local imports
-    var _=Npm.require("underscore");
-    var fs=Npm.require("fs");
-    var path=Npm.require("path");
-    // helper function, walks recursively inside nested folders and return absolute filenames
-    function walk(folder){
-        var filenames=[];
-        // get relative filenames from folder
-        var folderContent=fs.readdirSync(folder);
-        // iterate over the folder content to handle nested folders
-        _.each(folderContent,function(filename){
-            // build absolute filename
-            var absoluteFilename=folder+path.sep+filename;
-            // get file stats
-            var stat=fs.statSync(absoluteFilename);
-            if(stat.isDirectory()){
-                // directory case => add filenames fetched from recursive call
-                filenames=filenames.concat(walk(absoluteFilename));
-            }
-            else{
-                // file case => simply add it
-                filenames.push(absoluteFilename);
-            }
-        });
-        return filenames;
-    }
-    // save current working directory (something like "/home/user/projects/my-project")
-    var cwd=process.cwd();
-    // chdir to our package directory
-    process.chdir("packages"+path.sep+packageName);
-    // launch initial walk
-    var result=walk(folder);
-    // restore previous cwd
-    process.chdir(cwd);
-    return result;
-}
-
+// Get package assets recursively
+var getPackageAssets=function(a,b){function f(a){var b=[],g=d.readdirSync(a);return c.each(g,function(c){var g=a+e.sep+c,h=d.statSync(g);h.isDirectory()?b=b.concat(f(g)):b.push(g)}),b}var c=Npm.require("underscore"),d=Npm.require("fs"),e=Npm.require("path"),g=process.cwd();process.chdir("packages"+e.sep+a);var h=f(b);return process.chdir(g),h};
 
 Package.on_use(function (api) {
   api.use([
